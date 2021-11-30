361178ddbd7883f226172b64d9dac921a5d15ff9
Henit Chobisa <henitchobisa0885@gmail.com>
2021-11-30 15:54:57 +0530

    Round One Finish Added

--- routes/RoundOne/RoundOne.js
+++ routes/RoundOne/RoundOne.js
@@ -4,6 +4,7 @@ const router = express.Router();
 router.use('/submitResponse', require('./Routes/submitAnswers'));
 router.use('/getQuestions', require('./Routes/getQuestions'));
 router.use('/createQuestion', require('./Routes/createQuestion'));
+router.use('/finishRoundOne', require('./Routes/roundOneCompleted'));
 
 
 module.exports = router;
