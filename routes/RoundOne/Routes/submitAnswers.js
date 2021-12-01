12b0ac30c58b9d7a4231ce2eb33acfa38194670b
Henit Chobisa <henitchobisa0885@gmail.com>
2021-12-01 13:20:08 +0530

    sm s

--- routes/RoundOne/Routes/submitAnswers.js
+++ routes/RoundOne/Routes/submitAnswers.js
@@ -24,7 +24,7 @@ router.post("/", async (req, res) => {
         const isAnswerCorrect = await correctEnvironment.compareEnvironment(responseEnvironment);
     
         if (isAnswerCorrect == true){
-            team.AttemptsDone = 1;
+            team.AttemptsDone = 0;
             if (team.RoundOneAttemptedQuestions.length == 5){
                 team.RoundOneAttempted = true;
             }
@@ -32,9 +32,9 @@ router.post("/", async (req, res) => {
             await team.addPoints(attempts);
         }
         else {
-            team.AttemptsLeft = attempts;
+            team.AttemptsDone = attempts;
             if (attempts == 3){
-                team.AttemptsDone = 1;
+                team.AttemptsDone = 0;
                 if (team.RoundOneAttemptedQuestions.length == 5){
                     team.RoundOneAttempted = true;
                 }
